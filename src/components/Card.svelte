edd9c0234b654aa7fa3f28a461bd8737313fd836
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-06-23 23:00:41 -0300

    PreDeploy

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -5,6 +5,10 @@
 
     import {blur} from 'svelte/transition';
 
+
+    import { likeCount } from '../store/store.js';
+
+
     export let username;
     export let location;
     export let photo;
@@ -25,6 +29,11 @@
 
     function handleLike() {
       like = !like;
+      if(like){
+        likeCount.update(n => n + 1);
+      } else {
+        likeCount.update(n => n - 1);
+      }
     }
 
 </script>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,9 @@
+<script>
+    import {likeCount} from '../store/store.js';
 
+
+
+</script>
 <style>
   .Header {
     background-color: white;
@@ -70,6 +75,7 @@
                 <ul>
                     <li>
                         <i class="fas fa-heart" />
+                        {$likeCount === 0 ? '' : $likeCount} 
                     </li>
                     <li>
                         <i class="fas fa-user-alt" />
